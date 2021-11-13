c954374ae68cc5437c7914d23420965b3a8f4ecc
Mirek Rusin <mirek@me.com>
2021-11-13 18:39:40 +0100

    Adding support for esm.

--- test/permutations.test.ts
+++ test/permutations.test.ts
@@ -1,4 +1,4 @@
-import * as A from '../index.js'
+import * as A from '../cjs/index.js'
 
 const p = _ => Array.from(A.permutations(_.split(''))).map(_ => _.join(''))
 
--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,4 +1,4 @@
-import * as A from '../index.js'
+import * as A from '../cjs/index.js'
 
 test('deleteSwapRandom', () => {
   const a = A.indices(1000)
